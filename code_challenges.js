74a0f1bad8c1c486bd3f17f40f08c96109a9e991
dantelyon <dightmisc@gmail.com>
2019-12-20 13:49:43 +0100

    completed tailswap challenge

--- code_challenges.js
+++ code_challenges.js
@@ -1,3 +1,37 @@
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 20/12/19 //
+
+function tailSwap(arr){
+			let firstString = 
+				arr[0].slice(0, arr[0].indexOf(":")) + 
+				arr[1].slice(arr[1].indexOf(":"))
+			let secondString = 
+				arr[1].slice(0, arr[1].indexOf(":")) +
+				arr[0].slice(arr[0].indexOf(":"))
+			return [firstString, secondString]
+		}
+
+
+// 19/12/19 //
+
 		const myarray = [64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060]
 		function mean(input1, input2){
 			return input2.reduce((a, b) => a + b, 0) / input1
