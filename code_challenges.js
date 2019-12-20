fc8be328a05f894c97cd4ad1114e4a6f3a2656bf
dantelyon <dightmisc@gmail.com>
2019-12-20 14:11:23 +0100

    completed colorOf challenge

--- code_challenges.js
+++ code_challenges.js
@@ -14,29 +14,32 @@
 
 
 
-
+// 20/12/19 //
+	function colorOf(r,g,b){
+		const hex = d => Number(d).toString(16).padStart(2, '0')
+	  	return "#" + hex(r) + hex(g) + hex(b)
+	  
+	}
 
 
 // 20/12/19 //
-
-function tailSwap(arr){
-			let firstString = 
-				arr[0].slice(0, arr[0].indexOf(":")) + 
-				arr[1].slice(arr[1].indexOf(":"))
-			let secondString = 
-				arr[1].slice(0, arr[1].indexOf(":")) +
-				arr[0].slice(arr[0].indexOf(":"))
-			return [firstString, secondString]
-		}
+	function tailSwap(arr){
+		let firstString = 
+			arr[0].slice(0, arr[0].indexOf(":")) + 
+			arr[1].slice(arr[1].indexOf(":"))
+		let secondString = 
+			arr[1].slice(0, arr[1].indexOf(":")) +
+			arr[0].slice(arr[0].indexOf(":"))
+		return [firstString, secondString]
+	}
 
 
 // 19/12/19 //
-
-		const myarray = [64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060]
-		function mean(input1, input2){
+	const myarray = [64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060]
+	function mean(input1, input2){
 			return input2.reduce((a, b) => a + b, 0) / input1
 		}
-		function median(arr){
+	function median(arr){
 			arr = arr.sort((a, b) => a - b);
 			var middle = arr[Math.floor(arr.length / 2)];
 			var middle2 = arr[Math.floor((arr.length - 1) / 2)];
