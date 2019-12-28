85fa716b816c7dba074ef5c8f3b1653af069726a
dantelyon <dightmisc@gmail.com>
2019-12-28 14:03:20 +0100

    completed Multiples of 3 or 5

--- code_challenges.js
+++ code_challenges.js
@@ -4,6 +4,16 @@
 
 
 
+// 28/12/19 //
+	function solution(number){
+		let sum = 0
+		for (let i=0; i<number; i++){
+			if (i%3==0 || i%5==0) sum+=i
+		}
+		return sum
+	}
+
+
 // 27/12/19 //
 	function descendingOrder(n){
 		return Number( n.toString().split("").sort((a, b)=>{return b-a}).join("") )
