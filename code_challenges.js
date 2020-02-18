c1e7375dd81cdd9c1f78ee616e22f37ca10eeed6
dantelyon <dightmisc@gmail.com>
2020-02-18 15:57:20 +0100

    Find the divisors!

--- code_challenges.js
+++ code_challenges.js
@@ -3,6 +3,15 @@
 
 
 
+// Find the divisors! //
+	function divisors(integer) {
+		let divisors = []
+		for (let i=2; i<integer; i++)
+			if (integer%i==0) divisors.push(i);
+		return divisors.length? divisors : integer+" is prime"
+	}
+
+
 // Regex validate PIN code //
 	function validatePIN (pin) {
 		let isPINdigits = /^\d+$/.test(pin);
